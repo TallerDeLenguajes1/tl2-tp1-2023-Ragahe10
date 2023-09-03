b0c8255d86414fb271add3994be22e807a6345e1
RAMIRO <ragahe@gmail.com>
2023-09-02 21:21:49 -0300

    op reasignar

--- Interfaz.cs
+++ Interfaz.cs
@@ -1,7 +1,7 @@
 namespace Interfaz;
 
 using System;
-using System.Runtime.InteropServices;
+using System.Linq;
 using EspacioCadeteria;
 static class Interfaz{
     private static void EscribirMensaje(string message){
@@ -118,7 +118,7 @@ static class Interfaz{
                             CambiarEstado(Cadeteria);
                             break;
                         case 3:
-                            Reasignar();
+                            Reasignar(Cadeteria);
                             break;
                         case 4:
                             salir = 1;
@@ -162,33 +162,46 @@ static class Interfaz{
 
         EscribirMensaje("- Confirma el pedido???(presione enter, s o y, para confirmar)");
         ConsoleKeyInfo op = Console.ReadKey();
-        if(op.Key== ConsoleKey.S || op.Key== ConsoleKey.Y || op.Key== ConsoleKey.Enter){
-            p = Cadeteria.TomarPedido(nombre,direccion, telefono, datosRef,observacion);
+        if(op.Key== ConsoleKey.S || op.Key== ConsoleKey.Y || op.Key== ConsoleKey.Enter)
+        {
+            p = Cadeteria.TomarPedido(nombre, direccion, telefono, datosRef, observacion);
             EscribirMensaje("- Pedido creado...");
             Console.ReadKey();
             Console.Clear();
-            Console.WriteLine(Centrar(">>>ASIGNAR PEDIDO<<<",30));
-            Console.WriteLine("-> LISTADO DE CADETES:");
-            foreach (var c in Cadeteria.Cadetes)
-            {
-                Console.WriteLine(" ->ID:"+c.Id+", "+c.Nombre+" Cantidad de pedidos: "+c.Pedidos.Count());
-            }
-            EscribirMensaje("- Elija el ID del cadete a asignar:");
-            int.TryParse(Console.ReadLine(), out id);
-            Console.Clear();
-            if(id < Cadeteria.Cadetes.Count() && id > 0){
-                Cadeteria.AsignarPedido(id,p);
-                EscribirMensaje("- Pedido asignado...");
-            }else{
-                EscribirMensaje("- ID no valido, se asignará automáticamente...");
-                var r = new Random();
-                id = r.Next(1,Cadeteria.Cadetes.Count());
-                Cadeteria.AsignarPedido(id,p);
-            }
-        }else{
+            Asignar(p, Cadeteria);
+        }
+        else
+        {
             EscribirMensaje("- Pedido cancelado...");
         }
     }
+
+    private static void Asignar(Pedido p, Cadeteria Cadeteria)
+    {
+        int id;
+        Console.WriteLine(Centrar(">>>ASIGNAR PEDIDO<<<", 30));
+        Console.WriteLine("-> LISTADO DE CADETES:");
+        foreach (var c in Cadeteria.Cadetes)
+        {
+            Console.WriteLine(" ->ID:" + c.Id + ", " + c.Nombre + " Cantidad de pedidos: " + c.Pedidos.Count());
+        }
+        EscribirMensaje("- Elija el ID del cadete a asignar:");
+        int.TryParse(Console.ReadLine(), out id);
+        Console.Clear();
+        if (id < Cadeteria.Cadetes.Count() && id > 0)
+        {
+            Cadeteria.AsignarPedido(id, p);
+            EscribirMensaje("- Pedido asignado...");
+        }
+        else
+        {
+            EscribirMensaje("- ID no valido, se asignará automáticamente...");
+            var r = new Random();
+            id = r.Next(1, Cadeteria.Cadetes.Count());
+            Cadeteria.AsignarPedido(id, p);
+        }
+    }
+
     private static void CambiarEstado(Cadeteria Cdria)
     {
         int numPed = 0;
@@ -196,83 +209,120 @@ static class Interfaz{
         EscribirMensaje("- Ingrese número del pedido:");
         int.TryParse(Console.ReadLine(), out numPed);
         Console.Clear();
-        if(numPed >0 && numPed <= Cdria.NumPed){
-            var pedido = Cdria.Cadetes.SelectMany(cad=> cad.Pedidos).FirstOrDefault(ped=> ped.Numero == numPed);
-            Console.WriteLine(Centrar(">>>CAMBIAR ESTADO<<<",30));
-            Console.WriteLine(Centrar("PEDIDO "+pedido.Numero,30));
-            Console.WriteLine(" ->Estado: "+pedido.Estado.ToString());
-            Console.WriteLine(" ->Cliente: "+pedido.Client.Nombre);
+        if(numPed >0 && numPed <= Cdria.NumPed)
+        {
+            Pedido pedido = BuscarPedidoPorNum(Cdria, numPed);
+            Console.WriteLine(Centrar(">>>CAMBIAR ESTADO<<<", 30));
+            Console.WriteLine(Centrar("PEDIDO " + pedido.Numero, 30));
+            Console.WriteLine(" ->Estado: " + pedido.Estado.ToString());
+            Console.WriteLine(" ->Cliente: " + pedido.Client.Nombre);
 
             var key = new ConsoleKeyInfo();
             int op = 3, salir = 0;
-            do{
-            Console.WriteLine(Centrar(">>>CAMBIAR ESTADO<<<",30));
-            Console.WriteLine(Centrar("PEDIDO "+pedido.Numero,30));
-            Console.WriteLine(" ->Estado: "+pedido.Estado.ToString());
-            Console.WriteLine(" ->Cliente: "+pedido.Client.Nombre);
-            switch(pedido.Estado){
-                case Estado.SinEntregar:
-                    if(op==1){
-                        Console.WriteLine(Centrar(">>Cancelar<<",30));
-                    }else{
-                        Console.WriteLine(Centrar("Cancelar",30));
-                    }
-                    if(op==2){
-                        Console.WriteLine(Centrar(">>Entregar<<",30));
-                    }else{
-                        Console.WriteLine(Centrar("Entregar",30));
-                    }
-                    break;
-                case Estado.Entregado:
-                    Console.WriteLine("El pedido ya fué entregado.");
-                    break;
-                case Estado.Cancelado:
-                    Console.WriteLine("El pedido ya fué dado de baja.");
-                    break;
-            }
-            if(op==3){
-                Console.WriteLine(Centrar(">>Salir<<",30));
-            }else{
-                Console.WriteLine(Centrar("Salir",30));
-            }
-            key = Console.ReadKey();
-            Console.Clear();
-            switch (key.Key)
-            {   
-                case ConsoleKey.UpArrow:
-                    if(op>1 && pedido.Estado == Estado.SinEntregar){
-                        op--;
-                    }
-                    break;
-                case ConsoleKey.DownArrow:
-                    if(op<3 && pedido.Estado == Estado.SinEntregar){
-                        op++;
-                    }
-                    break;
-                case ConsoleKey.Enter:
-                    switch (op)
-                    {   
-                        case 1:
-                            pedido.CancelarPedido();
-                            break;
-                        case 2:
-                            pedido.EntregarPedido();
-                            break;
-                        case 3:
-                            salir = 1;
-                            break;
-                    }
-                    break;
-            }
-        } while (key.Key != ConsoleKey.Escape && salir==0);
+            do
+            {
+                Console.WriteLine(Centrar(">>>CAMBIAR ESTADO<<<", 30));
+                Console.WriteLine(Centrar("PEDIDO " + pedido.Numero, 30));
+                Console.WriteLine(" ->Estado: " + pedido.Estado.ToString());
+                Console.WriteLine(" ->Cliente: " + pedido.Client.Nombre);
+                switch (pedido.Estado)
+                {
+                    case Estado.SinEntregar:
+                        if (op == 1)
+                        {
+                            Console.WriteLine(Centrar(">>Cancelar<<", 30));
+                        }
+                        else
+                        {
+                            Console.WriteLine(Centrar("Cancelar", 30));
+                        }
+                        if (op == 2)
+                        {
+                            Console.WriteLine(Centrar(">>Entregar<<", 30));
+                        }
+                        else
+                        {
+                            Console.WriteLine(Centrar("Entregar", 30));
+                        }
+                        break;
+                    case Estado.Entregado:
+                        Console.WriteLine("El pedido ya fué entregado.");
+                        break;
+                    case Estado.Cancelado:
+                        Console.WriteLine("El pedido ya fué dado de baja.");
+                        break;
+                }
+                if (op == 3)
+                {
+                    Console.WriteLine(Centrar(">>Salir<<", 30));
+                }
+                else
+                {
+                    Console.WriteLine(Centrar("Salir", 30));
+                }
+                key = Console.ReadKey();
+                Console.Clear();
+                switch (key.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (op > 1 && pedido.Estado == Estado.SinEntregar)
+                        {
+                            op--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (op < 3 && pedido.Estado == Estado.SinEntregar)
+                        {
+                            op++;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        switch (op)
+                        {
+                            case 1:
+                                pedido.CancelarPedido();
+                                break;
+                            case 2:
+                                pedido.EntregarPedido();
+                                break;
+                            case 3:
+                                salir = 1;
+                                break;
+                        }
+                        break;
+                }
+            } while (key.Key != ConsoleKey.Escape && salir == 0);
 
-        }else{
+        }
+        else
+        {
             EscribirMensaje("- El número no corresponde a un pedido existente...");
         }
     }
-    private static void Reasignar()
+
+    private static Pedido BuscarPedidoPorNum(Cadeteria Cdria, int numPed)
     {
-        
+        return Cdria.Cadetes.SelectMany(cad => cad.Pedidos).FirstOrDefault(ped => ped.Numero == numPed);
     }
 
+    private static void Reasignar(Cadeteria Cdria)
+    {
+        int numPed = 0;
+        Console.WriteLine(Centrar(">>>REASIGNAR PEDIDO<<<",30));
+        EscribirMensaje("- Ingrese número del pedido:");
+        int.TryParse(Console.ReadLine(), out numPed);
+        Console.Clear();
+        if(numPed>0 && numPed<Cdria.NumPed){
+            var ped = BuscarPedidoPorNum(Cdria,numPed);
+            if(ped.Estado != Estado.Cancelado && ped.Estado != Estado.Entregado){
+                Asignar(ped,Cdria);
+            }else{
+                if(ped.Estado == Estado.Cancelado){
+                    EscribirMensaje("- No se puede reasignar porque ya fué cancelado");
+                }else{
+                    EscribirMensaje("- No se puede reasignar porque ya fué entregado");
+                }
+            }
+        }
+    }
 }
